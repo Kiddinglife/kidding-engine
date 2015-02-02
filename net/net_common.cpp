a1b99333d42351946eb4d251687ee5e907a2dace
Kiddinglife <mengdi.zhang@hotmail.com>
2015-02-03 10:51:38 +1100

    up trace_message_packet done

--- net/net_common.cpp
+++ net/net_common.cpp
@@ -54,7 +54,7 @@ void TRACE_MESSAGE_PACKET(bool isrecv, Packet* pPacket,
 		} else
 		{
 			ACE_DEBUG(( LM_INFO,
-				"\n%M::{%s} msgname:{%s}, msgID:{%d}, currMsgLength:{%d}, addr:{%s}\n",
+				"\n%M::{%s} msgname:{%s}, msgID:{%d}, currMsgPayloadLength:{%d}, addr:{%s}\n",
 				( isrecv == true ) ? "====>" : "<====",
 				pCurrMsgHandler->name_.c_str(), pCurrMsgHandler->msgID_, length, addr ));
 		}
