3cf66dc3aed41d9b44d5c9caf58f626a256e03d0
Kiddinglife <mengdi.zhang@hotmail.com>
2015-02-26 22:05:14 +1100

    up

---
+++ common/Profile.cpp
@@ -0,0 +1,82 @@
+﻿#include "Profile.h"
+ACE_KBE_BEGIN_VERSIONED_NAMESPACE_DECL
+
+Profile::Profile(std::string name, Profiles* profiles)
+{
+}
+
+
+Profile::~Profile()
+{
+}
+
+void Profile::start(void)
+{
+	TRACE("Profile::start()");
+
+	if( !initWatcher_ && stopCounts_ > 10 )
+	{
+		initializeWatcher();
+	}
+
+	TimeStamp now = timestamp();
+
+	// 记录第几次处理
+	if( inProgress_++ == 0 )
+	{
+		lastTime_ = now;
+	}
+
+	Profiles::ProfilesType& stack = pProfiles_->stack_;
+	if( !stack.empty() )
+	{
+		Profile* profile = stack.back();
+		profile->lastInternalTime_ = now - profile->lastInternalTime_;
+		profile->sumInternalTime_ += profile->lastInternalTime_;
+	}
+
+	stack.push_back(this);
+	lastInternalTime_ = now;
+
+	TRACE_RETURN_VOID();
+}
+
+void Profile::stop(ACE_UINT32 qty)
+{
+	TRACE("Profile::stop()");
+
+	TimeStamp now = timestamp();
+
+	/// 如果为0则表明自己是调用栈的产生着 在此我们可以得到这个函数总共耗费的时间
+	if( --inProgress_ == 0 )
+	{
+		lastTime_ = now - lastTime_;
+		sumInternalTime_ += lastTime_;
+	}
+
+	lastVal2Stop_ = qty;
+	sumAllVals2Stop_ += qty;
+	++stopCounts_;
+
+	Profiles::ProfilesType& stack = pProfiles_->stack_;
+	stack.pop_back();
+
+	// 得到本函数调用所耗费的时间
+	lastInternalTime_ = now - lastInternalTime_;
+	sumInternalTime_ += lastInternalTime_;
+
+	// 我们需要在此重设上一个函数中的profile对象的最后一次内部时间
+	// 使其能够在start时正确得到自调用完本函数之后进入下一个profile函数中时所消耗
+	// 的时间片段
+	if( !stack.empty() )
+		stack.back()->lastInternalTime_ = now;
+
+	TRACE_RETURN_VOID();
+}
+
+bool Profile::initializeWatcher(void)
+{
+	TRACE("Profile::initializeWatcher()");
+	TRACE_RETURN(true);
+}
+ACE_KBE_END_VERSIONED_NAMESPACE_DECL
---
+++ common/Profile.h
@@ -0,0 +1,43 @@
+﻿#include "ace\pre.h"
+#include "common.h"
+#include "timestamp.hpp"
+
+ACE_KBE_BEGIN_VERSIONED_NAMESPACE_DECL
+
+struct Profile
+{
+	std::string            strName_;
+	Profiles*              pProfiles_;
+	ACE_INT32		   inProgress_;  	// 记录第几次处理, 如递归等
+	bool                    initWatcher_;
+
+	static TimeStamp warningPeriod_;
+	TimeStamp          sumInternalTime_; 	                 /// Total time counted
+	TimeStamp          lastTime_;                   /// time after start
+	TimeStamp          lastInternalTime_; 	/// the last internal time
+
+	ACE_UINT32		   lastVal2Stop_;	///< The last value passed into stop.
+	ACE_UINT32		   sumAllVals2Stop_;	///< The total of all values passed into stop.
+	ACE_UINT32		   stopCounts_;			///< The number of times stop has been called.
+
+	Profile(std::string name="NULL", Profiles* profiles = NULL);
+	virtual ~Profile();
+
+	void start(void);
+	void stop(ACE_UINT32 qty = 0);
+
+	bool initializeWatcher(void);
+};
+
+struct Profiles
+{
+	typedef std::vector<Profile*> ProfilesType;
+	ProfilesType profiles_;
+	ProfilesType stack_;
+
+	Profiles();
+	virtual ~Profiles();
+};
+
+ACE_KBE_END_VERSIONED_NAMESPACE_DECL
+#include "ace/post.h"
